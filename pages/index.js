03e62cb7be23da41ba7552a2a370fc71443debc0
fpaco974 <136788267+fpaco974@users.noreply.github.com>
2024-04-06 08:50:29 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,15 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Le Comparateur Mariage</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <iframe
-          src="https://gamma.app/embed/kaq1nuch91xqz85"
+          src="https://gamma.app/embed/dx6ffp881ka6p3t"
           style={{ width: '100%', height: '100vh', border: 'none' }}
-          allowFullScreen
+          allow="FullScreen"
         ></iframe>
       </main>
 
